568bccf3451338f571891e3751ce4956e469a9cb
Steve Dowling <steve.dowling@fishtownanalytics.com>
2022-08-29 19:03:43 +0000

    removed fat

--- models/example/python/sample_python_model.py
+++ models/example/python/sample_python_model.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def model(dbt, session):
 
-    # dbt.config(materialized="table")
+    dbt.config(materialized="table")
 
 
     df = pd.DataFrame(np.random.randn(100, 4), columns=list('ABCD'))
