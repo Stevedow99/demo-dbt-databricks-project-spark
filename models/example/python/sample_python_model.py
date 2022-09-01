98409c8cf2fdea0ac8aca448671d8c407ab4fb35
Steve Dowling <steve.dowling@fishtownanalytics.com>
2022-09-01 17:04:35 +0000

    updates

--- models/example/python/sample_python_model.py
+++ models/example/python/sample_python_model.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import numpy as np
 
 def model(dbt, session):
-    dbt.config(materialized='table', packages=['pandas'])
-    upstream_model = dbt.ref('stg_tpch_customers')
-    return upstream_model
+
+    dbt.config(materialized='table')
+
+    base_model = pd.DataFrame(np.random.randn(100, 4), columns=list('ABCD'))
+
+    return base_model
